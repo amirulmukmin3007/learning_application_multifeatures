aff4caa48a81fb61eb6ce16ff3fd0edfdac49eec
amirulmukmin3007 <amirulmukmin878@gmail.com>
2025-08-29 17:19:21 +0800

    remove localhost

--- lib/utils/route/api.dart
+++ lib/utils/route/api.dart
@@ -1,6 +1,5 @@
 class Api {
-  // static const baseUrl = 'https://keapitest.mooo.com/';
-  static const baseUrl = 'http://10.0.2.2:8000/';
+  static const baseUrl = 'https://keapitest.mooo.com/';
 
   static const getTaskDataPeter = baseUrl + 'api/tasks/peter';
   static const getTaskDataSteven = baseUrl + 'api/tasks/steven';
